71a2c7b8025747d69407ca514c0b1911dc5cad8f
App Academy Student <student@appacademy.io>
2015-08-28 17:01:16 -0700

    broke minesweeper:-/

--- board.rb
+++ board.rb
@@ -4,16 +4,47 @@ require 'byebug'
 
 class Board
   attr_reader :grid, :size, :bombs
+  attr_accessor :possible_bombs
+
 
   def initialize(size, bombs)
     @grid = Array.new(size) { Array.new(size) }
     @size = size
     @bombs = bombs
+    @possible_bombs = size**2
     populate
+  end
+
+  def check(pos)
+    return if self[pos].reveal == "_" || self[pos].reveal != "*"
+    self[pos].reveal = "_"
+
+
+    (-1..1).each do |x_offset|
+      (-1..1).each do |y_offset|
+        neighbor_pos = [pos[0] + x_offset, pos[1] + y_offset]
+        next if neighbor_pos == pos
+        bomb_count = find_bomb_count(neighbor_pos)
+        # x = x_offset + pos[0]
+        # y = y_offset + pos[1]
+
+        # debugger
+        if !pos_on_board?(neighbor_pos) || self[neighbor_pos].bomb
+          next
+        elsif bomb_count > 0
+          self[neighbor_pos].reveal = bomb_count
+          # return
+        else
+          check(neighbor_pos)
+          # return
+        end
+      end
+    end
   end
 
   def display
     header = (0...size).to_a.join(" ")
+    puts "#{@possible_bombs} LEFT"
     puts "  #{header}".colorize(:blue)
     self.grid.each_with_index do |row, i|
       display_row(row, i)
@@ -62,6 +93,44 @@ class Board
     total_bombs
   end
 
+  def possible_bomb_count
+    # @possible_bombs
+    counter = 0
+
+    grid.each do |row|
+      row.each do |tile|
+        counter += 1 if tile.reveal = "_" || tile.reveal.is_a?(Fixnum)
+      end
+    end
+    @possible_bombs = size**2 - counter
+  end
+
+  def pos_on_board?(pos)
+    pos.all? { |pos| (0...size).include?(pos) }
+  end
+
+  def random_pos
+    result = []
+    2.times { result << rand(size) }
+    result
+  end
+
+  def [](pos)
+    x,y = pos
+    grid[x][y]
+  end
+
+  def []=(pos, value)
+    x,y = pos
+    grid[x][y] = value
+  end
+
+  private
+end
+
+
+
+
 
   # def find_bomb_locations(pos)
   #   bomb_placements = []
@@ -86,26 +155,3 @@ class Board
   #       y = y_offset + pos[1]
   #       if pos_on_board?([x, y]) && !used_spaces.include?[x,y]
   #
-
-  def pos_on_board?(pos)
-    pos.all? { |pos| (0...size).include?(pos) }
-  end
-
-  def random_pos
-    result = []
-    2.times { result << rand(size) }
-    result
-  end
-
-  def [](pos)
-    x,y = pos
-    grid[x][y]
-  end
-
-  def []=(pos, value)
-    x,y = pos
-    grid[x][y] = value
-  end
-
-  private
-end
--- game.rb
+++ game.rb
@@ -4,7 +4,7 @@ require_relative 'board'
 class Game
   attr_reader :board
 
-  def initialize(size = 9, bombs = 0)
+  def initialize(size = 9, bombs = 4)
     @board = Board.new(size, bombs)
   end
 
@@ -27,37 +27,24 @@ class Game
     if bomb_count > 0
       board[move].reveal = bomb_count
     else
-      computer_check(move)
+      board.check(move)
       #board[move].reveal = "_"
     end
   end
 
-  def computer_check(pos)
-    board[pos].reveal = "_"
-
-    (-1..1).each do |x_offset|
-      (-1..1).each do |y_offset|
-        x = x_offset + pos[0]
-        y = y_offset + pos[1]
-        bomb_count = board.find_bomb_count([x, y])
-        if board.pos_on_board?([x, y]) && bomb_count > 0
-          board[x, y].reveal = bomb_count
-          return
-        else
-          return if board.pos_on_board?([x, y]) == false
-          computer_check([x, y])
-        end
-      end
-    end
-
-  end
-
   def play
     while true
+
       system("clear")
+      board.possible_bomb_count
       board.display
       single_move
+
     end
+  end
+
+  def won?
+
   end
 
 
